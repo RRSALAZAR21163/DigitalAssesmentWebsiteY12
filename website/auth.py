df0757d0f19eac5730edf2bd187b72bd864f40b6
RRSALAZAR21163 <21163@my.sanctamaria.school.nz>
2024-04-04 13:20:38 +1300

    GET, METHODS, POSTS. REQUESTS
    REQUESTS, POSTS, METHODS. Login page and Sign Up page

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,36 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", text="Testing")
+    data = request.form
+    print(data)
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        
+        if len(email) < 4:
+            pass
+        elif len(first_name) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 8:
+            pass
+        else:
+            pass
+            # add user to database
+        
     return render_template("signup.html")
