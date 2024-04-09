c0ce878fd0270afa1f7961f90a933c94ae5d8fcb
RRSALAZAR21163 <21163@my.sanctamaria.school.nz>
2024-04-09 12:30:50 +1200

    SQLALCHEMY + DATABASE SETUP
    Flask SQLAlchemy Setup, Foreign Key relationships. Setup Database Models. And creating Database

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from .models import User
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -12,7 +14,7 @@ def login():
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/sign-up', methods=['GET', 'POST'])
+@auth.route("/signup", methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
@@ -20,17 +22,15 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         
-        
         if len(email) < 4:
-            pass
+            flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
-            pass
-        elif password1 != password2:
-            pass
+            flash('First name must be greater than 1 character.', category='error')
         elif len(password1) < 8:
-            pass
+            flash('Password must have at least 8 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match.', category='error')
         else:
-            pass
-            # add user to database
+            flash('Account created!', category='success')
         
     return render_template("signup.html")
