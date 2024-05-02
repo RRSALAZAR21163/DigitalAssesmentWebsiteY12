d577148085957f21c649a3e71f8bfbd59fc7f669
RRSALAZAR21163 <21163@my.sanctamaria.school.nz>
2024-05-03 10:59:24 +1200

    User Authentication + Login Module
    User Authentication. Login Module. Logout Button.

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 from .models import User
 from . import db
 auth = Blueprint("auth", __name__)
@@ -16,13 +17,14 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash("Logged in successfully!", category='success')
-                return redirect(url_for('views.home'))
+                login_user(user, remember=True)
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
             flash('Email does not exist.', category='error')
 
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
+
 
 
 @auth.route("/signup", methods = ['GET', 'POST'])
@@ -51,4 +53,10 @@ def sign_up():
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
     
-    return render_template("signup.html")
+    return render_template("signup.html", user=current_user)
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
