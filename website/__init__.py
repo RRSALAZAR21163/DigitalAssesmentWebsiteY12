7e4e4d5d77f365844f44037815df27fab54605b9
RRSALAZAR21163 <21163@my.sanctamaria.school.nz>
2024-06-10 14:51:57 +1200

    Home page template + Other
    Home page template + added some other stuff that could be for future

--- website/__init__.py
+++ website/__init__.py
@@ -13,18 +13,17 @@ def create_app():
     db.init_app(app)
     
     
-    
     from .views import views
     from .auth import auth
     
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     
-    from .models import User, Note
+    from .models import User, Note, Post
     
     with app.app_context():
         db.create_all()
-        
+    
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
--- website/models.py
+++ website/models.py
@@ -1,18 +1,32 @@
 from . import db
 from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
-    first_name = db.Column(db.String(150))
+    username = db.Column(db.String(150))
     password = db.Column(db.String(150))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     notes = db.relationship('Note')
+    posts = db.relationship('Post', backref='user', passive_deletes=True)
+    
+    
+    #function to return a string when adds something
+    def __repr__(self):
+        return '<Name %r>' %self.id
     
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+   
    
