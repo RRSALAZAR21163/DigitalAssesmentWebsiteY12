bab5fc04b6c52038923dcc80694ac52a48b7ac3b
RRSALAZAR21163 <21163@my.sanctamaria.school.nz>
2024-06-18 12:27:40 +1200

    Blogs v1 - In the beginning
    Created Blog Models for database
    Created Blog view page
    Created Blog 'create a post/blog' page
    Added View blog page to navbar
    Added some fixes
    Added routes for blogs current and for future additions

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,11 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Flask, Blueprint, render_template, redirect, url_for, request, flash, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
 from .models import User
+from .models import Note
 from . import db
+import json
+
 auth = Blueprint("auth", __name__)
 
 
@@ -27,16 +30,36 @@ def login():
     return render_template("login.html", user=current_user)
 
 @auth.route("/dashboard", methods = ['GET', 'POST'])
-@login_required
 def dashboard():
+    if request.method == 'POST':
+        note = request.form.get('note') #Gets the note from the HTML
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id) #providing the schema for the note
+            db.session.add(new_note) #adding the note to the database
+            db.session.commit()
+            flash('Note added!', category='success')
     return render_template("dashboard.html", user=current_user)
 
+@auth.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+            #flash
+            flash('Note deleted!', category='success')
+    return jsonify({})
 
 @auth.route("/signup", methods = ['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        first_name = request.form.get('firstName')
+        username = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
     
@@ -45,14 +68,14 @@ def sign_up():
             flash('Email already exists.', category='error')
         elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
-        elif len(first_name) < 2:
+        elif len(username) < 2:
             flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
             flash('Passwords do not match.', category='error')
         elif len(password1) < 8:
             flash('Password must have at least 8 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password = generate_password_hash(password1, method='scrypt:32768:8:1'))
+            new_user = User(email=email, username=username, password = generate_password_hash(password1, method='scrypt:32768:8:1'))
             db.session.add(new_user)
             db.session.commit()
             flash('Account created!', category='success')
--- website/views.py
+++ website/views.py
@@ -1,5 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, request, redirect, flash, jsonify
 from flask_login import login_required, current_user
+from .models import User
+from .models import Post
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
@@ -8,3 +12,36 @@ views = Blueprint('views', __name__)
 @views.route("/home")
 def home():
     return render_template("home.html", user=current_user)
+
+@views.route("/blogs", methods = ['GET', 'POST'])
+def blog():
+    posts = Post.query.all()
+    return render_template("blogs.html", user=current_user, posts=posts)
+    
+@views.route("/create-post", methods = ['GET', 'POST'])
+@login_required
+def create_post():
+    if request.method == "POST":
+        text = request.form.get('text')
+        
+        if not text:
+            flash('Post cannot be empty', category='error')
+        else:
+            post = Post(text=text, author=current_user.id)
+            db.session.add(post)
+            db.session.commit()
+            flash('Post created!', category='success')
+            return redirect(url_for('views.blog'))
+    return render_template('addblogs.html', user=current_user)
+
+@views.route("/posts/<username>")
+@login_required
+def posts(username):
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
+        flash('No user with that username exists.', category='error')
+        return redirect(url_for('views.home'))
+
+    posts = Post.query.filter_by(author=user.id).all()
+    return render_template("posts.html", user=current_user, posts=posts, username=username)
