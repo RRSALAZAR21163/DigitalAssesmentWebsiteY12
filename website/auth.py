971e152ec8a22ffb7f521f905e170f6f0973093e
RRSALAZAR21163 <21163@my.sanctamaria.school.nz>
2024-08-07 09:38:09 +1200

    Code Comment feature V2
    Added more comments. More detailed comments

--- website/auth.py
+++ website/auth.py
@@ -9,59 +9,59 @@ auth = Blueprint("auth", __name__)
 
 
 # Login route
-@auth.route("/login", methods = ['GET', 'POST'])
+@auth.route("/login", methods = ['GET', 'POST']) #Method is GET and POST To get details from database and to post/update details to database. ROUTES
 def login(): #define the function
     if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
+        email = request.form.get('email') #get details from database
+        password = request.form.get('password') #get details from database
         
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash("Logged in successfully!", category='success')
-                login_user(user, remember=True)
-                return redirect(url_for('views.dashboard'))
+                flash("Logged in successfully!", category='success') #Flash message
+                login_user(user, remember=True) #remember user if user logs in
+                return redirect(url_for('views.dashboard')) #redirect back to dashboard page when user logs in
             else:
-                flash('Incorrect password, try again.', category='error')
+                flash('Incorrect password, try again.', category='error') #Flash message
         else:
-            flash('Email does not exist.', category='error')
+            flash('Email does not exist.', category='error') #Flash message
 
     return render_template("login.html", user=current_user)
 
 # Signup route
-@auth.route("/signup", methods = ['GET', 'POST'])
+@auth.route("/signup", methods = ['GET', 'POST']) #Method is GET and POST. To get details from database and to post/update details to database. ROUTES
 def sign_up():  #define the function
-    if request.method == 'POST':
+    if request.method == 'POST': #Post. Update these details to the database
         email = request.form.get('email')
         username = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
     
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(email=email).first() 
         if user:
-            flash('Email already exists.', category='error')
-        elif len(email) < 4:
-            flash('Email must be greater than 3 characters.', category='error')
-        elif len(username) < 2:
-            flash('First name must be greater than 1 character.', category='error')
+            flash('Email already exists.', category='error') #Flash message
+        elif len(email) < 4: #if email is less than 3 characters
+            flash('Email must be greater than 3 characters.', category='error') #Flash message
+        elif len(username) < 2: #if username is less than 1 character
+            flash('First name must be greater than 1 character.', category='error') #Flash message
         elif password1 != password2:
             flash('Passwords do not match.', category='error')
-        elif len(password1) < 8:
-            flash('Password must have at least 8 characters.', category='error')
+        elif len(password1) < 8: #passwords must have atleast 8 characters
+            flash('Password must have at least 8 characters.', category='error') #Flash message
         else:
-            new_user = User(email=email, username=username, password = generate_password_hash(password1, method='scrypt:32768:8:1'))
+            new_user = User(email=email, username=username, password = generate_password_hash(password1, method='scrypt:32768:8:1')) #generate a hashed password. Encrypt the password
             db.session.add(new_user) # add something to database
             db.session.commit() #commit to database
-            login_user(new_user, remember=True)
-            flash('Account created!', category='success')
+            login_user(new_user, remember=True) #if new user create a new user, and remember
+            flash('Account created!', category='success') #Flash message 
             return redirect(url_for('views.home'))
     
     return render_template("signup.html", user=current_user)
 
 #Logout route
-@auth.route("/logout")
+@auth.route("/logout") #ROUTES
 @login_required #authentication/user is logged in is required to access this page
 def logout():  #define the function
-    logout_user()
-    flash('You have logged out Successfully!', category='success')
-    return redirect(url_for('auth.login'))
+    logout_user() #logout the user function
+    flash('You have logged out Successfully!', category='success') #Flash message 
+    return redirect(url_for('auth.login')) #redirect back to login page
--- website/views.py
+++ website/views.py
@@ -15,134 +15,134 @@ views = Blueprint('views', __name__)
 #HOME ROUTE
 #USER HAS TO BE AUTHENTICATED TO ACCESS
 @login_required #authentication/user is logged in is required to access this page
-@views.route("/")
-@views.route("/home")
+@views.route("/") #routes
+@views.route("/home") #ROUTES
 def home(): #define the function
     return render_template("home.html", user=current_user) #render template home.html
 
 #DASHBOARD ROUTE
-@views.route("/dashboard", methods = ['GET', 'POST'])
+@views.route("/dashboard", methods = ['GET', 'POST']) #Method is GET and POST. To get details from database and to post/update details to database. ROUTES
 @login_required #authentication/user is logged in is required to access this page
 def dashboard(): #define the function
     if request.method == 'POST': #request 
         note = request.form.get('note') #Gets the note from the HTML
-        if len(note) < 1:
-            flash('Note is too short!', category='error')
+        if len(note) < 1: #if the input of letters or words is less than one
+            flash('Note is too short!', category='error') #Flash message
         else:
             new_note = Note(data=note, user_id=current_user.id) #providing the schema for the note
             db.session.add(new_note) #adding the note to the database
             db.session.commit()  #commit to database
-            flash('Note added!', category='success')
+            flash('Note added!', category='success') #Flash message
     return render_template("dashboard.html", user=current_user) #render template dashboard.html
 
 #DELETING NOTES ROUTE
-@views.route('/delete-note', methods=['POST'])
+@views.route('/delete-note', methods=['POST']) #Method is POST. Update detail/informations on database. ROUTES
 def delete_note(): #define the function
     note = json.loads(request.data) #variable
     noteId = note['noteId']
     note = Note.query.get(noteId)
     if note:
-        if note.user_id == current_user.id:
+        if note.user_id == current_user.id: #if user is the creater user can delete
             db.session.delete(note) # delete something to database
             db.session.commit()  #commit to database
             #flash
-            flash('Note deleted!', category='success')
+            flash('Note deleted!', category='success') #Flash message
     return jsonify({})
 
 #BLOG ROUTE
-@views.route("/blogs", methods = ['GET', 'POST'])
+@views.route("/blogs", methods = ['GET', 'POST']) #Method is GET and POST. ROUTES
 @login_required
 def blog(): #define the function
-    posts = Post.query.all()
+    posts = Post.query.all() #display posts
     return render_template("blogs.html", user=current_user, posts=posts) #render template blogs.html
 
 
 #CREATE POST
-@views.route("/create-post", methods=['GET', 'POST'])
+@views.route("/create-post", methods=['GET', 'POST']) #Method is GET and POST. To get details from database and to post/update details to database. ROUTES
 @login_required #authentication/user is logged in is required to access this page
 def create_post(): #define the function
-    if request.method == "POST":
-        text = request.form.get('text')
+    if request.method == "POST": #POST
+        text = request.form.get('text') #form. 'Text'
 
-        if not text:
-            flash('Post cannot be empty', category='error')
-        else:
-            post = Post(text=text, author=current_user.id)
+        if not text: #if the post is empty
+            flash('Post cannot be empty', category='error') #Flash message
+        else: 
+            post = Post(text=text, author=current_user.id) #create new post. Matches the author id with current user id
             db.session.add(post) # add something to database
             db.session.commit()  #commit to database
-            flash('Post created!', category='success')
+            flash('Post created!', category='success') #Flash message
             return redirect(url_for('views.blog'))
 
     return render_template('addblogs.html', user=current_user) #render template addblogs.html
 
 #DELETING POSTSS
-@views.route("/delete-post/<id>")
+@views.route("/delete-post/<id>") # ROUTES
 @login_required #authentication/user is logged in is required to access this page
 def delete_post(id): #define the function
     post = Post.query.filter_by(id=id).first()
 
-    if not post:
-        flash("Post does not exist.", category='error')
-    elif current_user.id != post.author:
-        flash('You do not have permission to delete this post.', category='error')
+    if not post: #post not existing
+        flash("Post does not exist.", category='error') #Flash message
+    elif current_user.id != post.author: #if current user id is = to the post.author.
+        flash('You do not have permission to delete this post.', category='error') #Flash message
     else:
         db.session.delete(post) # delete something to database
         db.session.commit()  #commit to database
-        flash('Post deleted.', category='success')
+        flash('Post deleted.', category='success') #Flash message
 
     return redirect(url_for('views.blog')) #redirect to views.blog
 
 # USER SPECIFIC PAGE ROUTE.
-@views.route("/posts/<username>")
+@views.route("/posts/<username>") # ROUTES
 @login_required #authentication/user is logged in is required to access this page
 def posts(username): #define the function
     user = User.query.filter_by(username=username).first()
 
     if not user:
-        flash('No user with that username exists.', category='error')
-        return redirect(url_for('views.home'))
+        flash('No user with that username exists.', category='error') #Flash message
+        return redirect(url_for('views.home')) #redirect back to home page
     posts = user.posts
     return render_template("userposts.html", user=current_user, posts=posts, username=username) #render template userposts.html
 
 #CREATING COMMENTS 
-@views.route("/create-comment/<post_id>", methods=['POST'])
+@views.route("/create-comment/<post_id>", methods=['POST']) #Method is POST. Update details on database. ROUTES
 @login_required #authentication/user is logged in is required to access this page
 def create_comment(post_id): #define the function
     text = request.form.get('text')
 
     if not text:
-        flash('Comment cannot be empty.', category='error')
+        flash('Comment cannot be empty.', category='error') #Flash message
     else:
         post = Post.query.filter_by(id=post_id)
         if post:
-            comment = Comment(text=text, author=current_user.id, post_id=post_id)
+            comment = Comment(text=text, author=current_user.id, post_id=post_id) #create new comment
             db.session.add(comment) # add something to database
             db.session.commit()  #commit to database
-            flash('Post created!', category='success')
+            flash('Post created!', category='success') #Flash message
         else:
-            flash('Post does not exist.', category='error')
+            flash('Post does not exist.', category='error') #Flash message
 
     return redirect(url_for('views.blog')) #redirect to views.blogs
 
 #DELETEING COMMENTS
-@views.route("/delete-comment/<comment_id>")
+@views.route("/delete-comment/<comment_id>")#ROUTES
 @login_required #authentication/user is logged in is required to access this page
 def delete_comment(comment_id): #define the function
     comment = Comment.query.filter_by(id=comment_id).first()
 
     if not comment:
-        flash('Comment does not exist.', category='error') 
-    elif current_user.id != comment.author and current_user.id != comment.post.author:
-        flash('You do not have permission to delete this comment.', category='error')
+        flash('Comment does not exist.', category='error')  #Flash message. Error message
+    elif current_user.id != comment.author and current_user.id != comment.post.author: #checks if userid does not equals to comment.author and userid does not equal commentpostauthor.
+        flash('You do not have permission to delete this comment.', category='error') #Flash message
     else:
         db.session.delete(comment) #delete something to database
         db.session.commit()  #commit to database
-        flash('Post deleted.', category='success')
+        flash('Post deleted.', category='success') #Flash message
 
     return redirect(url_for('views.blog')) #redirect to views.blog
 
 #LIKING POSTS
-@views.route("/like-post/<post_id>", methods=['POST'])
+@views.route("/like-post/<post_id>", methods=['POST']) #Method is POST. Update details on Database. ROUTES
 @login_required #authentication/user is logged in is required to access this page
 def like(post_id): #define the function
     post = Post.query.filter_by(id=post_id).first()
@@ -150,7 +150,7 @@ def like(post_id): #define the function
         author=current_user.id, post_id=post_id).first()
 
     if not post:
-        return jsonify({'error': 'Post does not exist.'}, 400)
+        return jsonify({'error': 'Post does not exist.'}, 400) #Sends Error if Post does not exist
     elif like:
         db.session.delete(like) # delete something to database
         db.session.commit() #commit to database
